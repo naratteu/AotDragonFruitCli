cfe27ad56e794174443bb56bb39d1549419631e7
박성원 <skfhddlg@naver.com>
2024-01-12 16:43:22 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    static async Task Main(string name = "World")
+    {
+        Console.Write("Hello, ..");
+        await Task.Delay(1000);
+        Console.WriteLine($"{name}!");
+    }
+}
